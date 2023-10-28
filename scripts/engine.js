23b1fff5db56e19838ab16c39b53e955b9ee9892
Tomás <tomaschavesdev@gmail.com>
2023-10-27 22:10:29 -0300

    feat: add keyboard funcionality

--- scripts/engine.js
+++ scripts/engine.js
@@ -11,3 +11,7 @@ pianoKeys.forEach((key) => {
     console.log(key.dataset.key);  
     key.addEventListener("click", () => playTune(key.dataset.key))
 });
+
+document.addEventListener("keydown", (event) => {
+  playTune(event.key);
+});
