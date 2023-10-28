fa74d3d86a643ddfa29b9dd8f4db6eb8844333c6
Tomás <tomaschavesdev@gmail.com>
2023-10-27 22:08:26 -0300

    feat: add keys and key tones

---
+++ scripts/engine.js
@@ -0,0 +1,13 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("./tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `./tunes/${key}.wav`;
+    audio.play();
+};
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key);  
+    key.addEventListener("click", () => playTune(key.dataset.key))
+});
